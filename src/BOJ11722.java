bf0c098653222f046f0f94bc5b29d94e5a07ec10
낙낙 <cnh6123@naver.com>
2019-11-03 20:52:23 +0900

    11/3 commit

--- src/BOJ11722.java
+++ src/BOJ11722.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class BOJ11722 {
+public class BOJ11722{
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         int size;
