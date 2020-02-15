ce4afd52a00f201d95d975fe1fa810805f2bf8d8
낙낙 <cnh6123@naver.com>
2020-02-16 01:04:26 +0900

    2/16 commit (3) (완주하지 못한 선수 해쉬를 이용한 코드 추가)

--- src/Programmers42576.java
+++ src/Programmers42576.java
@@ -22,6 +22,7 @@ public class Programmers42576 {
         }
         return answer;
     }
+
     //효율성 에러,  두 번째 생각한 방법
     //두 배열을 정렬한 후, 다르면 그 값을 출력하거나 끝까지 남은 part배열 마지막 값 출력
     //정렬은 안될 줄 알았는데 효율성 통과
@@ -35,5 +36,21 @@ public class Programmers42576 {
                 return participant[i];
         }
         return participant[i];
+    }
+
+    //해쉬를 참고해서 해결한 코드.(다른 사람의 코드 참고)
+    //HashMap : 키와 쌍을 이용해 키를 검색해서 값을 찾아낼 수 있는 유용한 자료구조? 알고리즘.
+    //HashMap.getOrDefault(a,b)는 a라는 키에 대한 값이 없을 경우, 뒤의 b를 default로 한다.
+    //HashMap.keySet()은 맵에 저장된 키들을 중복 없이 꺼낼 수 있는 메소드이다.
+    public String solution3(String[] participant, String[] completion) {
+        Map<String,Integer> hashMap = new HashMap<>();
+        for(String player : participant)
+            hashMap.put(player, hashMap.getOrDefault(player,0)+1);
+        for(String player : completion)
+            hashMap.put(player, hashMap.get(player)-1);
+        for(String key : hashMap.keySet())
+            if(hashMap.get(key)!=0)
+                return key;
+        return null;
     }
 }
