1499a62b34e85cdae97b42ea80b76377df1f6e9d
낙낙 <cnh6123@naver.com>
2020-02-19 00:35:14 +0900

    2/19 commit (프로그래머스 가장 큰 수)

---
+++ src/Programmers42746.java
@@ -0,0 +1,41 @@
+import java.util.*;
+//프로그래머스 코딩테스트 연습 <정렬> - '가장 큰 수' 문제
+public class Programmers42746 {
+    public static String solution(int[] numbers) {
+        String answer = "";
+
+        //int 배열을 String 배열로 변환
+        String[] arr = new String[numbers.length];
+        for (int i = 0; i < numbers.length; i++) {
+            //Integer.toString()은 없다.
+            //String.valueOf를 이용해서 int->String 변환
+            arr[i] = (String.valueOf(numbers[i]));
+        }
+
+        //배열 정렬, 정렬 규칙으로는 2개를 더하여 더 큰 쪽이 우선순위가 있도록 정렬
+        //compare()와 compareTo()의 차이
+        //Arrays.sort() 두번째 매개변수 Comparator<> 이용법
+        Arrays.sort(arr, new Comparator<String>() {
+            @Override
+            public int compare(String s1, String s2) {
+                System.out.println("s2+s1 " + s2+s1 + " s1+s2 " + s1+s2);
+                System.out.println("compareTo " + (s2+s1).compareTo(s1+s2));
+                return (s2+s1).compareTo(s1+s2);
+            }
+        });
+
+        //0000 처럼 0으로만 구성되어있으면 0 return
+        if (arr[0].equals("0")) return "0";
+
+        //그 외의 경우 순차적으로 연결하여 answer return
+        for (int i = 0; i < arr.length; i++) {
+            answer+=arr[i];
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        int [] numbers = {6,10,2};
+        System.out.println("solution " + solution(numbers));
+    }
+}
