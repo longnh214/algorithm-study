926f97abe0712201d522a84eccd31392f5fbe5c3
nakhoon <nakhoon@M800>
2020-11-04 16:49:47 +0900

    11/20 BOJ10942 time access (reference anyone's code)

--- src/BOJ10942.java
+++ src/BOJ10942.java
@@ -1,6 +1,6 @@
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;;
+import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 //팰린드롬 연습문제(시간 초과) !?!?!?
