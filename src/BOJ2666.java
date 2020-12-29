5f8a6eaab95bc82ebfc7eabe86a8aca294d5ce47
낙낙 <cnh6123@naver.com>
2020-12-29 23:44:32 +0900

    2020/12/29 commit (백준 <DFS/DP> '벽장문의 이동')

--- src/BOJ2666.java
+++ src/BOJ2666.java
@@ -1,62 +1,53 @@
+/**
+ * @author choi
+ * @date Dec 29, 2020
+ * @see https://www.acmicpc.net/problem/2666
+ * @mem 11,436kb
+ * @time 80ms
+ * @caution
+ * [고려사항]
+ * dp로 분류되어 있어서 어려웠던 문제로 옛날부터 풀지 못했던 문제지만, 완전탐색 dfs를 이용하여 문제를 해결할 수 있었다.
+ * [입력사항]
+ * [출력사항]
+ */
 import java.util.*;
-
-//https://lyzqm.blogspot.com/2017/03/2666.html 참고 어려움..
+import java.io.*;
+//백준 <BFS/DP> '벽장문의 이동'
 public class BOJ2666 {
-    public static void main(String[] args){
-        Scanner scan = new Scanner(System.in);
+    static int N, M, answer;
+    static int [] visited;
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int [][] dp = new int[21][21];//[벽장순서][나머지 index]
-        int size; //전체 벽장 크기
-        int [] op = new int[2]; //열린 문 좌표 저장
-        int count; //열어야하는 벽장 수
-        int [] quest;
+        N = Integer.parseInt(br.readLine());
 
-        size = scan.nextInt();
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        for(int i=0;i<op.length;i++){
-            op[i] = scan.nextInt();
+        int first = Integer.parseInt(st.nextToken());
+        int second = Integer.parseInt(st.nextToken());
+
+        M = Integer.parseInt(br.readLine());
+
+        visited = new int[M];
+
+        for(int i=0;i<M;i++) {
+            visited[i] = Integer.parseInt(br.readLine());
         }
 
-        quest = new int[size+1];
+        answer = N * M + 1;
+        dfs(first,second,0,0);
+        System.out.println(answer);
+    }
 
-        count = scan.nextInt();
+    public static void dfs(int first, int second, int depth, int moveCnt) {
+        // 분기한정
+        if (moveCnt > answer) return;
 
-        for(int i=1;i<=count;i++){
-            quest[i] = scan.nextInt();
+        if (depth == M) {
+            answer = Math.min(answer, moveCnt);
+            return;
         }
-
-        for(int i=1;i<21;i++){
-             for(int j=1;j<21;j++){
-                 dp[i][j] = -1;
-             }
-        }
-
-        dp[1][op[1]] = Math.abs(op[0] - quest[1]);
-        dp[1][op[0]] = Math.abs(op[1] - quest[1]);
-
-        for (int n = 1; n < count; n++){    //열어야하는 벽장 크기만큼(순서대로)
-            for (int m = 1; m <= size; m++){
-                if (dp[n][m] != -1 && quest[n] != m){
-                    int first, second;
-                    first = dp[n][m] + Math.abs(quest[n] - quest[n + 1]);
-                    second = dp[n][m] + Math.abs(m - quest[n + 1]);
-                    if (dp[n + 1][m] != -1){
-                        first = Math.min(dp[n + 1][m], first);
-                    }
-                    if (dp[n + 1][quest[n]] != -1){
-                        second = Math.min(dp[n + 1][quest[n]], second);
-                    }
-                    dp[n + 1][m] = first;
-                    dp[n + 1][quest[n]] = second;
-                }
-            }
-        }
-        int ans = 123456789;
-        for (int n = 1; n <= size; n++){
-            if (dp[count][n] != -1)
-                ans = Math.min(ans, dp[count][n]);
-        }
-        System.out.println(ans);
-        scan.close();
+        dfs(visited[depth], second, depth + 1, moveCnt + Math.abs(first - visited[depth]));
+        dfs(first, visited[depth], depth + 1, moveCnt + Math.abs(second - visited[depth]));
     }
 }
