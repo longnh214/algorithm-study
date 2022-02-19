48c86486bbbf2135ad27c7889b066f63f441ac8b
Nakhoon Choi <cnh6123@naver.com>
2022-02-19 15:26:31 +0900

    2022/02/19 commit (백준 <정렬> '좌표 정렬하기')

--- algorithm/src/BOJ11650.java
+++ algorithm/src/BOJ11650.java
@@ -1,11 +1,12 @@
 /**
- * @author choi
- * @date Aug 9, 2020
+ * @author nakhoon
+ * @date Feb 19, 2022
  * @see https://www.acmicpc.net/problem/11650
- * @mem 213,256kb
- * @time 2,648ms
+ * @mem 56,640kb
+ * @time 564ms
  * @caution
  * [고려사항]
+ * 리스트 정렬을 했을 때 시간 초과가 발생해서 2차원 배열을 정렬하는 방식으로 바꿔서 해결할 수 있었다.
  * [입력사항]
  * [출력사항]
  */
@@ -13,35 +14,29 @@ import java.util.*;
 import java.io.*;
 //백준 <정렬> - '좌표 정렬하기'
 public class BOJ11650 {
-    public static void main(String[] args) throws IOException{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-        ArrayList<Dot> list = new ArrayList<>();
-        for(int i=0;i<N;i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            int x = Integer.parseInt(st.nextToken());
-            int y = Integer.parseInt(st.nextToken());
-            list.add(new Dot(x,y));
-        }
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		int [][] pointArr = new int[N][2];
+		for(int i=0;i<N;i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			pointArr[i][0] = Integer.parseInt(st.nextToken());
+			pointArr[i][1] = Integer.parseInt(st.nextToken());
+		}
+		
+		Arrays.sort(pointArr, new Comparator<int []>() {
 
-        Collections.sort(list);
-
-        for(Dot dot : list) {
-            System.out.printf("%d %d\n",dot.x,dot.y);
-        }
-    }
-    static class Dot implements Comparable<Dot>{
-        int x;
-        int y;
-        public Dot(int x, int y) {
-            super();
-            this.x = x;
-            this.y = y;
-        }
-        @Override
-        public int compareTo(Dot o) {
-            if(this.x != o.x) return Integer.compare(this.x, o.x);
-            else return Integer.compare(this.y, o.y);
-        }
-    }
+			@Override
+			public int compare(int[] o1, int[] o2) {
+				// TODO Auto-generated method stub
+				if(o1[0] != o2[0]) return Integer.compare(o1[0], o2[0]);
+				else return Integer.compare(o1[1],o2[1]);
+			}
+		});
+		StringBuilder sb = new StringBuilder();
+		for(int i=0;i<N;i++) {
+			sb.append(pointArr[i][0]).append(" ").append(pointArr[i][1]).append("\n");
+		}
+		System.out.println(sb.toString());
+	}
 }
