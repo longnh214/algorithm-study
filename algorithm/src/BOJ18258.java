64dd880c34356b9560cbf6027dc06b9a5099100e
Nakhoon choi <cnh6123@naver.com>
2022-08-09 09:00:23 +0900

    2022/08/09 commit (백준 <큐> '큐 2')

--- algorithm/src/BOJ18258.java
+++ algorithm/src/BOJ18258.java
@@ -1,13 +1,14 @@
 /**
  * @author nakhoon
- * @date 2021, 12월 6일
+ * @date 2022/08/09
  * @see https://www.acmicpc.net/problem/18258
- * @mem 396,804kb
- * @time 1,528ms
+ * @mem 323,632kb
+ * @time 992ms
  * @caution
  * [고려사항]
- * System.out.println을 이용해서 출력이 발생하는 명령어에 대해 바로 바로 출력하려고 하면 시간 초과가 발생했다.
- * StringBuilder나 BufferedWriter를 이용했다면 시간 초과가 전혀 발생하지 않았을 것 이다.
+ * 평소 StringBuilder를 출력할 때, sb.substring(0, sb.length()-1)을 출력하는 습관이 있는데, 아무 것도 없는 출력이 있는 경우
+ * StringIndexOutOfBoundsException이 발생하는 문제가 있어서 수정했다.
+ * 큐를 직접 구현하는 방식으로 문제를 해결했다.
  * [입력사항]
  * [출력사항]
  */
@@ -15,60 +16,42 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
-
+import java.util.StringTokenizer;
 //백준 <큐> '큐 2'
 public class BOJ18258 {
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
         StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+        int queue[] = new int[N];
+        int front =0;
+        int rear = 0;
+        for(int i=0;i<N;i++) {
+            st = new StringTokenizer(br.readLine());
+            String input = st.nextToken();
 
-        Queue<Integer> q = new LinkedList<>();
-        int back = -1;
-        for(int i=0;i<N;i++){
-            String [] arr = br.readLine().split(" ");
-            int num = arr.length == 2 ? Integer.parseInt(arr[1]) : -1;
-
-            switch(arr[0]){
-                case "push":
-                    q.offer(num);
-                    back = num;
-                    break;
-                case "front":
-                    if(q.size() != 0){
-                        sb.append(q.peek()).append("\n");
-                    }else{
-                        sb.append(-1).append("\n");
-                    }
-                    break;
-                case "back":
-                    if(q.size() != 0){
-                        sb.append(back).append("\n");
-                    }else{
-                        sb.append(-1).append("\n");
-                    }
+            switch(input) {
+                case "empty":
+                    sb.append(front == rear ? 1 : 0).append("\n");
                     break;
                 case "size":
-                    sb.append(q.size()).append("\n");
+                    sb.append(rear - front).append("\n");
                     break;
-                case "empty":
-                    sb.append(q.isEmpty() ? 1 : 0).append("\n");
+                case "front":
+                    sb.append(front == rear ? -1 : queue[front]).append("\n");
+                    break;
+                case "back":
+                    sb.append(front == rear ? -1 : queue[rear - 1]).append("\n");
                     break;
                 case "pop":
-                    if(q.size() != 0){
-                        sb.append(q.poll()).append("\n");
-                    }else{
-                        sb.append(-1).append("\n");
-                    }
-
-                    if(q.size() == 0){
-                        back = -1;
-                    }
+                    sb.append(front == rear ? -1 : queue[front++]).append("\n");
                     break;
+                case "push":
+                    int num = Integer.parseInt(st.nextToken());
+                    queue[rear++] = num;
             }
         }
-        System.out.println(sb.substring(0, sb.length()-1));
+        System.out.print(sb);
     }
 }
