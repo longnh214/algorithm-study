388e2d7e5101c9a55a2d3507fb3edf31c456da9f
Nakhoon Choi <cnh6123@naver.com>
2022-09-17 12:13:45 +0900

    2022/09/17 commit (백준 <수학> '마지막 팩토리얼 수')

---
+++ algorithm/src/BOJ2553.java
@@ -0,0 +1,42 @@
+/**
+ * @author nakhoon
+ * @date 2022, 9월 17일
+ * @see https://www.acmicpc.net/problem/2553
+ * @mem
+ * @time
+ * @caution
+ * [고려사항]
+ * [입력사항]
+ * [출력사항]
+ */
+import java.io.*;
+//백준 <수학> '마지막 팩토리얼 수'
+public class BOJ2553 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        int [] dp = new int[N+1];
+
+        int temp;
+
+        dp[1] = 1;
+        dp[2] = 2;
+        dp[3] = 6;
+        dp[4] = 4;
+
+        for(int i=5;i<=N;i++){
+            if(i % 5 == 0){
+                temp = i/5;
+                dp[i] = (dp[temp] * (int)Math.pow(2, temp%4))%10;
+            }else{
+                temp = (i/5)*5;
+                dp[i] = dp[temp];
+                for(int j=temp+1; j<=i; j++) {
+                    dp[i] = dp[i] * j % 10;
+                }
+            }
+        }
+        System.out.println(dp[N]);
+    }
+}
