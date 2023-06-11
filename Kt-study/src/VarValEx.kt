42a2644bd9eee9106b3593b60788c4727381b23c
Nakhoon choi <cnh6123@naver.com>
2023-06-11 20:09:52 +0900

    2023/06/11 commit (Kotlin Var, Val Example)

---
+++ Kt-study/src/VarValEx.kt
@@ -0,0 +1,8 @@
+fun main(){
+    var number1 = 10L //바꿀 수 있는 변수
+    val number2 = 10L //바꿀 수 없는 변수
+    number1 = 5L
+//    number2 = 20L // 값이 새로 assign 될 수 없다.
+    println(number1)
+    println(number2)
+}
