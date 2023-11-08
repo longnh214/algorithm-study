e761e29620cd8e9f0ad84bc08808e7067ae3ed21
Nakhoon choi <cnh6123@naver.com>
2023-11-08 12:34:56 +0900

    2023/11/8 commit (프로그래머스 <연습문제> '약수의 합')

---
+++ algorithm/src/Programmers12928.java
@@ -0,0 +1,30 @@
+/**
+ * @author nakhoon
+ * @date 11/9/23
+ * @see https://school.programmers.co.kr/learn/courses/30/lessons/12928
+ * @mem
+ * @time
+ * @caution
+ * [고려사항]
+ * [입력사항]
+ * [출력사항]
+ */
+import java.util.*;
+import java.io.*;
+
+//프로그래머스 <연습문제> '약수의 합'
+public class Programmers12928 {
+    public static void main(String[] args) {
+        int n = 12;
+        System.out.println(solution(n));
+    }
+    public static int solution(int n) {
+        int answer = 0;
+        for(int i=1;i<=n;i++){
+            if(n % i == 0){
+                answer += i;
+            }
+        }
+        return answer;
+    }
+}
