87b736afecddbcb90b539a21f32753f62e32b9c8
Nakhoon choi <cnh6123@naver.com>
2024-01-03 08:54:37 +0900

    2024/1/3 commit (프로그래머스 <연습문제> '숫자의 표현')

---
+++ algorithm/src/Programmers12924.java
@@ -0,0 +1,46 @@
+/**
+ * @author nakhoon
+ * @date 1/3/24
+ * @see https://school.programmers.co.kr/learn/courses/30/lessons/12924
+ * @mem
+ * @time
+ * @caution
+ * [고려사항]
+ * [입력사항]
+ * [출력사항]
+ */
+
+//프로그래머스 <연습문제> '숫자의 표현'
+public class Programmers12924 {
+    public static void main(String[] args) {
+        int n = 10000;
+        System.out.println(solution(n));
+    }
+
+    public static int solution(int n) {
+        int[] sum = new int[n + 1];
+        sum[0] = 0;
+
+        //등차수열 배열 값 지정.
+        for (int i = 1; i <= n; i++) {
+            sum[i] = sum[i - 1] + i;
+        }
+
+        int count = 0;
+        int startIdx = 0;
+
+        for (int i = 1; i <= n; i++) {
+            int temp = sum[i] - sum[startIdx];
+            if (temp == n) {
+                count++;
+                startIdx++;
+                i = startIdx;
+            } else if (temp > n) {
+                startIdx++;
+                i = startIdx;
+            }
+        }
+
+        return count;
+    }
+}
