a6370a9d8f5d99f3dea74cb7d8164d50a52e3666
Nakhoon choi <cnh6123@naver.com>
2024-01-14 12:34:56 +0900

    2024/1/14 commit (백준 <입출력> '??!')

---
+++ algorithm/src/BOJ10926.kt
@@ -0,0 +1,18 @@
+/**
+ * @author nakhoon
+ * @date 2024/1/14
+ * @see https://www.acmicpc.net/problem/10926
+ * @mem
+ * @time
+ * @caution
+ * [고려사항]
+ * [입력사항]
+ * [출력사항]
+ */
+import java.util.*;
+import java.io.*;
+
+//백준 <입출력> '??!'
+fun main(args: Array<String>) = with(BufferedReader(InputStreamReader(System.`in`))) {
+    println(readLine() + "??!")
+}
