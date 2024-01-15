aa80aa9355f4c2d81e38222d781d68924c80c226
Nakhoon choi <cnh6123@naver.com>
2024-01-15 12:34:56 +0900

    2024/1/15 commit (백준 <입출력> '1998년생인 내가 태국에서는 2541년생?!')

---
+++ algorithm/src/BOJ18108.kt
@@ -0,0 +1,18 @@
+/**
+ * @author nakhoon
+ * @date 2024/1/15
+ * @see https://www.acmicpc.net/problem/18108
+ * @mem 12,188kb
+ * @time 88ms
+ * @caution
+ * [고려사항]
+ * [입력사항]
+ * [출력사항]
+ */
+import java.util.*;
+import java.io.*;
+
+//백준 <입출력> '1998년생인 내가 태국에서는 2541년생?!'
+fun main(args: Array<String>) = with(BufferedReader(InputStreamReader(System.`in`))) {
+    println(Integer.parseInt(readLine()) - 543)
+}
