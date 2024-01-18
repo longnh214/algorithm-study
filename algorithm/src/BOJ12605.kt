fb2df073a56d3e7d557b36c083aefb610c6a11a7
Nakhoon choi <cnh6123@naver.com>
2024-01-19 08:43:45 +0900

    2024/1/19 commit (백준 <문자열> '단어순서 뒤집기')

--- algorithm/src/BOJ12605.kt
+++ algorithm/src/BOJ12605.kt
@@ -14,6 +14,7 @@
 import java.util.*;
 import java.io.*;
 
+//백준 <문자열> '단어순서 뒤집기'
 fun main(args: Array<String>) = with(BufferedReader(InputStreamReader(System.`in`))) {
     val size = Integer.parseInt(readLine())
 
