0f3ac4920e2850a38138d2629e8454432c1d3752
Nakhoon choi <cnh6123@naver.com>
2024-01-23 08:11:04 +0900

    2024/1/23 commit (백준 <입출력> '고양이')

---
+++ algorithm/src/BOJ10171.kt
@@ -0,0 +1,22 @@
+/**
+ * @author nakhoon
+ * @date 2024/01/23
+ * @see https://www.acmicpc.net/problem/10171
+ * @mem 11,732kb
+ * @time 84ms
+ * @caution
+ * [고려사항]
+ * \를 출력할 때 앞에 백슬래시를 하나 더 붙여줘야하는 이슈가 있는 문제였다.
+ * [입력사항]
+ * [출력사항]
+ */
+import java.util.*;
+import java.io.*;
+
+//백준 <입출력> '고양이'
+fun main(args: Array<String>) = with(BufferedReader(InputStreamReader(System.`in`))) {
+    println("\\    /\\")
+    println(" )  ( ')")
+    println("(  /  )")
+    println(" \\(__)|")
+}
