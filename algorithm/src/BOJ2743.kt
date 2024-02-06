576791de3f8321ea14dcb105407ce751252d14a6
Nakhoon choi <cnh6123@naver.com>
2024-02-06 23:11:47 +0900

    2024/2/6 commit (백준 <문자열> '단어 길이 재기')

---
+++ algorithm/src/BOJ2743.kt
@@ -0,0 +1,18 @@
+/**
+ * @author naknak
+ * @date 2/6/24
+ * @see https://www.acmicpc.net/problem/2743
+ * @mem 12,168kb
+ * @time 88ms
+ * @caution
+ * [고려사항]
+ * [입력사항]
+ * [출력사항]
+ */
+import java.util.*;
+import java.io.*;
+
+//백준 <문자열> '단어 길이 재기'
+fun main(args: Array<String>) = with(BufferedReader(InputStreamReader(System.`in`))) {
+    println(readLine().length)
+}
