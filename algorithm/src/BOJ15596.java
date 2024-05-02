e76335d20a6277ada78fe7752872845a010525c1
Nakhoon Choi <cnh6123@naver.com>
2024-05-02 15:51:11 +0900

    2024/05/02 commit (백준 <수학> '정수 N개의 합')

--- algorithm/src/BOJ15596.java
+++ algorithm/src/BOJ15596.java
@@ -1,2 +1,22 @@
-package PACKAGE_NAME;public class BOJ15596 {
+/**
+ * @author nakhoonchoi
+ * @date 2024/05/02
+ * @see https://www.acmicpc.net/problem/15596
+ * @mem 423,048kb
+ * @time 24ms
+ * @caution
+ * [고려사항]
+ * [입력사항]
+ * [출력사항]
+ */
+
+//백준 <수학> '정수 N개의 합'
+public class BOJ15596 {
+    long sum(int[] a) {
+        long sum = 0;
+        for (int i = 0; i < a.length; i++) {
+            sum += a[i];
+        }
+        return sum;
+    }
 }
