0f168feb0a8b72b2e99c939759e93d265685916d
Nakhoon Choi <cnh6123@naver.com>
2024-05-04 21:39:05 +0900

    2024/05/04 commit (백준 <수학> '진법 변환')

---
+++ algorithm/src/BOJ2745.kt
@@ -0,0 +1,23 @@
+/**
+ * @author nakhoonchoi
+ * @date 2024/05/04
+ * @see https://www.acmicpc.net/problem/2745
+ * @mem 12,524kb
+ * @time 96ms
+ * @caution
+ * [고려사항]
+ * String.toInt(redix) 형식의 출력을 통해 진법 변환을 하였다.
+ * [입력사항]
+ * [출력사항]
+ */
+import java.util.*;
+import java.io.*;
+
+//백준 <수학> '진법 변환'
+fun main(args: Array<String>) = with(BufferedReader(InputStreamReader(System.`in`))) {
+    val st = StringTokenizer(readLine())
+    val str = st.nextToken()
+    val redix = st.nextToken().toInt()
+
+    println(str.toInt(redix))
+}
