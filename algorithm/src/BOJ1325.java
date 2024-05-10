250536f0123869ff1fd4a5d270c9cb21b573e46e
Nakhoon Choi <cnh6123@naver.com>
2024-05-10 14:03:15 +0900

    2024/05/10 commit (백준 <BFS> '효율적인 해킹')

--- algorithm/src/BOJ1325.java
+++ algorithm/src/BOJ1325.java
@@ -1,60 +1,97 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
+/**
+ * @author nakhoonchoi
+ * @date 2024/05/10
+ * @see https://www.acmicpc.net/problem/1325
+ * @mem 206,564kb
+ * @time 2,792ms
+ * @caution
+ * [고려사항]
+ * 21년도에 풀었던 문제인데, 시간이 지나고 보니 시간 초과, 실패 처리되어있던 문제이다.
+ * 그 전에는 dfs로 풀었는데 bfs로 바꾸어서 풀었고, 그래프를 인접 배열이 아닌
+ * 이중 인접 리스트로 구현하였다.
+ * [입력사항]
+ * [출력사항]
+ */
+import java.io.*;
+import java.util.*;
 
+//백준 <BFS> '효율적인 해킹'
 public class BOJ1325 {
-    static int n;
-    static int m;
-    static ArrayList<Integer>[] a;
-    static boolean [] visited;
-    static int [] dp;
-    static int max = Integer.MIN_VALUE;
-    public static void main(String[] args) throws IOException {
+    static int N, M, max;
+    static int [] counts;
+    static boolean [][] visited;
+    static List<List<Integer>> adjList;
+
+    public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String [] str = br.readLine().split(" ");
-        n = Integer.parseInt(str[0]);
-        m = Integer.parseInt(str[1]);
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        a = new ArrayList[n+1];
-        dp = new int[n+1];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
 
-        for(int i=1;i<=n;i++){
-            a[i] = new ArrayList<Integer>();
+        max = 0;
+        visited = new boolean[N + 1][N + 1];
+        adjList = new ArrayList<>();
+        counts = new int[N + 1];
+        for (int i=0;i<=N;i++) {
+            adjList.add(new ArrayList<>());
         }
 
-        for(int i=0;i<m;i++){
-            str = br.readLine().split(" ");
-            int x = Integer.parseInt(str[0]);
-            int y = Integer.parseInt(str[1]);
-            a[x].add(y);
+        for (int i=0;i<M;i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            adjList.get(b).add(a);
         }
 
-        for(int i=1;i<=n;i++){
-            visited = new boolean[n+1];
-            dfs(i);
+        StringBuilder sb = new StringBuilder();
+
+        for (int i=1;i<=N;i++) {
+            bfs(i);
         }
 
-        for(int i=1;i<=n;i++){
-            max = Math.max(max,dp[i]);
+        for (int i=1;i<=N;i++) {
+            if (counts[i] == max) {
+                sb.append(i).append(' ');
+            }
         }
 
-        for(int i=1;i<=n;i++){
-            if(max == dp[i])
-                System.out.format("%d ",i);
-        }
-        System.out.println();
+        bw.write(sb.substring(0, sb.length()-1));
+        bw.close();
+        br.close();
     }
 
-    static void dfs(int cur){
-        visited[cur] = true;
-        //각 정점을 기준으로 탐색되어지는 정점은 현재 기준 정점을 해킹할 수 있다는 것을 의미
-        //next의 dp 값을 올려준 뒤에 dfs 함수를 재귀 호출한다.
-        for(int next : a[cur]){
-           if(!visited[next]){
-               dp[next]++;
-               dfs(next);
-           }
+    private static void bfs(int node) {
+        ArrayDeque<Integer> q = new ArrayDeque<>();
+
+        q.offer(node);
+
+        while (!q.isEmpty()) {
+            int cur = q.poll();
+
+            if (visited[node][cur]) continue;
+            visited[node][cur] = true;
+            counts[node]++;
+
+            for (int next : adjList.get(cur)) {
+                if (visited[node][next]) continue;
+
+                if (next > node) {
+                    q.offer(next);
+                } else {
+                    for (int i=1;i<=N;i++) {
+                        if (visited[node][i]) continue;
+                        if (visited[next][i]) {
+                            visited[node][i] = true;
+                            counts[node]++;
+                        }
+                    }
+                }
+            }
         }
+
+        max = Math.max(max, counts[node]);
     }
 }
