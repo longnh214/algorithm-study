4511c66d917ea735c3bff8f4e78e87f7e002c11d
Nakhoon Choi <cnh6123@naver.com>
2025-01-13 20:36:29 +0900

    2025/01/13 commit (LeetCode <LeetCode 75> 'Find Peak Element')

--- algorithm/src/LeetCode162.java
+++ algorithm/src/LeetCode162.java
@@ -30,9 +30,9 @@ public class LeetCode162 {
     public int findPeakRecursion(int [] nums, int left, int right){
         while(left < right){
             int mid = (left + right) / 2;
-            if(mid == 0 && nums[mid] > nums[mid + 1]){
+            if(mid == 0 && nums[mid] > nums[mid + 1]){ //왼쪽 끝
                 return mid;
-            }else if(mid == nums.length - 1 && nums[mid] > nums[mid - 1]){
+            }else if(mid == nums.length - 1 && nums[mid] > nums[mid - 1]){ //오른쪽 끝
                 return mid;
             }else if(nums[mid] > nums[mid + 1] && nums[mid] > nums[mid - 1]){
                 return mid;
