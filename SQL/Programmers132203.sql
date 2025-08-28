de56b878ecf27935c422e9a20821858d6682fc4f
Nakhoon Choi <cnh6123@naver.com>
2025-08-28 14:28:53 +0900

    2025/08/28 commit (프로그래머스 <SELECT> '흉부외과 또는 일반외과 의사 목록 출력하기')

---
+++ SQL/Programmers132203.sql
@@ -0,0 +1,9 @@
+-- 프로그래머스 <SELECT> '흉부외과 또는 일반외과 의사 목록 출력하기'
+-- https://school.programmers.co.kr/learn/courses/30/lessons/132203
+-- DATE 2025/08/28
+
+SELECT DR_NAME, DR_ID, MCDP_CD, DATE_FORMAT(HIRE_YMD, '%Y-%m-%d')
+FROM DOCTOR
+WHERE MCDP_CD = 'CS' OR MCDP_CD = 'GS'
+ORDER BY HIRE_YMD DESC, DR_NAME ASC
+;
