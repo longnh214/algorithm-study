4083906f2080248c09ec85903dc90031005d62cb
Nakhoon Choi <cnh6123@naver.com>
2025-08-31 09:01:33 +0900

    2025/08/31 commit (프로그래머스 <SELECT> '인기있는 아이스크림')

---
+++ SQL/Programmers133024.sql
@@ -0,0 +1,8 @@
+-- 프로그래머스 <SELECT> '인기있는 아이스크림'
+-- https://school.programmers.co.kr/learn/courses/30/lessons/133024
+-- DATE 2025/08/31
+
+SELECT FLAVOR
+FROM FIRST_HALF
+ORDER BY TOTAL_ORDER DESC, SHIPMENT_ID ASC
+;
