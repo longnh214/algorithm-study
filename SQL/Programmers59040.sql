524d4882d5b2597512825c4a97843f41bee40a58
Nakhoon Choi <cnh6123@naver.com>
2025-09-07 15:24:19 +0900

    2025/09/07 commit (프로그래머스 <GROUP BY> '고양이와 개는 몇 마리 있을까')

---
+++ SQL/Programmers59040.sql
@@ -0,0 +1,9 @@
+-- 프로그래머스 <GROUP BY> '고양이와 개는 몇 마리 있을까'
+-- https://school.programmers.co.kr/learn/courses/30/lessons/59040
+-- DATE 2025/09/07
+
+SELECT ANIMAL_TYPE, COUNT(ANIMAL_TYPE) AS COUNT
+FROM ANIMAL_INS
+GROUP BY ANIMAL_TYPE
+ORDER BY ANIMAL_TYPE
+;
